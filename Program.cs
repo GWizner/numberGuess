6f8523f3f7d61b1da3db5e5fecafa293ca6a332f
gregory <wiznergregory@gmail.com>
2022-04-13 20:25:01 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿Random rand = new Random();
+
+
+bool realInteger = false;
+int userNumberGuess = 0;
+int randomNumber = rand.Next(20) + 1;
+bool correctGuess = false;
+int numGuesses = 0;
+
+
+        Console.WriteLine("Hello, User!");
+        Console.WriteLine("Welcome to our number guessing game.");
+while (correctGuess == false)
+{
+    numGuesses++;
+    Console.WriteLine("We have chosen a number between 1 and 20.");
+    Console.WriteLine();
+    Console.WriteLine("Can you guess what it is? ");
+    string userInput = Console.ReadLine();
+    realInteger = int.TryParse(userInput, out userNumberGuess);
+
+    if (realInteger == false)
+    {
+        Console.WriteLine("Huh?! O_o");
+        break;
+    }
+    else if (userNumberGuess < 1 || userNumberGuess > 20)
+    {
+        Console.WriteLine("Um... ONE TO TWENTY...");
+        continue;
+    }
+    else if (userNumberGuess < randomNumber || userNumberGuess > randomNumber)
+    {
+        Console.WriteLine("WRONG!!!");
+    }
+    else
+    {
+        Console.WriteLine("You got it!");
+    }
+}
+
+    Console.WriteLine();
+    if (realInteger == false)
+    {
+        Console.WriteLine("Funny...");
+    }
+    else if (numGuesses == 1)
+    {
+        Console.WriteLine("Wowsa, 1st try!!");
+    }
+
+    else
+    {
+        Console.WriteLine($"It took you {numGuesses} tries.");
+    }
+
+        Console.WriteLine();
+    Console.WriteLine("Press any key to exit.");
